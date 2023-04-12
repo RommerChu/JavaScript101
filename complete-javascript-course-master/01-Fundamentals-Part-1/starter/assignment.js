afe4a06a5dd4eb5d37af04a234b0dbf20805219c
Rommer Chu <rommer.chu@gmail.com>
2023-04-12 13:41:35 -0400

    my javascrip files

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,13 @@
+//Assignment #1
+console.log( "----------Assignment----------" )
+let country = 'Philippines';
+let continent = 'Asia';
+let population = 1500000;
+console.log( country + " " + continent + " " + population );
+
+//Data Types
+let isIsland = country == "Philippines";
+let language;
+console.log( isIsland, population, country, language )
+
+//Data type
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,96 @@
+/*
+let js = 'amazing';
+console.log( 54 + 3 - 10 / 2 );
+
+//console.log('Rommer');
+// console.log(23);
+
+let firstName = 'Peter';
+console.log( firstName + " is " + js );
+console.log( firstName );
+console.log( firstName );
+console.log( firstName );
+
+let person = 'Rommer';
+let PI = 3.14;
+
+console.log( "His name is ${firstName}" )
+
+let javascriptIsFun = true;
+console.log( javascriptIsFun );
+
+// console.log(typeof true);
+console.log( typeof javascriptIsFun );
+
+javascriptIsFun = "YES!";
+console.log( typeof javascriptIsFun );
+
+let year;
+console.log( typeof year );
+console.log( year );
+
+year = 1991;
+console.log( typeof year );
+
+console.log( typeof null );
+
+//use let for mutation
+let age = 30;
+age = 31;
+
+// constant declaration without changing is allowed
+const birthYear = 1991;
+
+lasName = "Chu"
+console.log( lasName );
+
+
+//Basic Operators
+//Math Operators
+const now = 2023;
+
+const ageRommer = now - 1972;
+const ageCorazon = now - 1971;
+console.log( ageRommer, ageCorazon );
+
+console.log( ageRommer * 2, ageRommer / 2, 2 ** 3 );
+// 2 ** 3 means 2 to the power of 3 = 2*2*2
+
+
+const firstName = "Rommer"
+const lastName = 'Chu';
+console.log( firstName + lastName );
+console.log( firstName + " " + lastName );
+
+//Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x += 4; // x = x + 4 = 15 + 10 + 4 = 29
+x++;
+x--;
+
+console.log( x )
+
+//Comparison operators
+console.log( ageRommer < ageCorazon ); // true
+console.log( ageRommer >= 51 ); // true
+
+const isFullAge = ageRommer >= 51; //true
+console.log( isFullAge ); // true
+
+console.log( now - 1972 > now - 1971 ); //false
+*/
+
+
+const now = 2023;
+const ageRommer = now - 1972;
+const ageCorazon = now - 1971;
+console.log( ageRommer, ageCorazon );
+console.log( now - 1972 > now - 1971 )
+
+let x, y;
+x = y = 25 + 10 - 5; // 30 30
+console.log( x, y );
+
+const averageAge = ( ageRommer + ageCorazon ) / 2;
+console.log( ageRommer, ageCorazon, averageAge );
