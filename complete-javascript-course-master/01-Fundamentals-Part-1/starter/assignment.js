4cdb8884b5c308f7a7d40e19ad28b80b4f4f66bf
Rommer Chu <rommer.chu@gmail.com>
2023-04-12 15:50:58 -0400

    update 2

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -16,3 +16,32 @@ const officialLang = language;
 language = "English";
 
 console.log( officialLang );
+
+//Coding Challenge #1
+//Formula 1
+// BMI = mass / height ** 2
+
+//Formula 2
+// BMI = mass / (height * height) (mass in kg and height in meter)
+
+//Case1
+// Mark > weigth 78, height 1.69
+// John > weight 92, height 1.95
+
+//Case 2
+// Mark > weight 95, height 1.88
+// John > weight 85, height 1.76
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+let bmiMark = massMark / heightMark ** 2;
+let bmiJohn = massJohn / heightJohn ** 2;
+
+console.log( bmiMark, bmiJohn )
+
+markHigherBMI = bmiMark > bmiJohn
+console.log( markHigherBMI )
