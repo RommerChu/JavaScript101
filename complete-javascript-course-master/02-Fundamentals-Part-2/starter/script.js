e563fc9778badcff67626f2c80a6c5d330c93a16
Rommer Chu <rommer.chu@gmail.com>
2023-04-17 14:34:05 -0400

    Funtion inside the Function

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -10,7 +10,7 @@ if ( hasDriversLic ) console.log( "I can drive 🚗" )
 const interface = 'Audio'; // Uncaight error result;
 */
 
-
+/*
 //--------------FUNCTION
 function logger() {
     console.log( "My name is Rommer" );
@@ -36,9 +36,10 @@ console.log( "AppleOrangeJuice: " + appleOrangeJuice )
 
 //const num = Number( '23' );
 //console.log( num )
+*/
 
 
-
+/*
 //----------Function Declaration
 //const age1 = calAge1( 1972 );  // Can place the initiazed variable before the function
 
@@ -49,9 +50,10 @@ function calAge1( birthYear ) {
 }
 const age1 = calAge1( 1972 );
 console.log( "Rommer's age is " + age1 );
+*/
 
 
-
+/*
 //----------Function Expression
 //const age2 = calAge2( 1972 ); // CANNOT initialized before the Expression
 
@@ -63,7 +65,10 @@ console.log( "Age now is " + age2 )
 
 //Print result from two functions
 console.log( age1, age2 )
+*/
 
+
+/*
 //-------------------------------my sample
 function toy( truck ) {
     const kidsToy = `The toy is a ${truck}`;
@@ -72,16 +77,21 @@ function toy( truck ) {
 //maleHuman( corazon );
 const thing = toy( 'payloader' );
 console.log( thing )
+*/
 
+
+/*
 //----------Arrow Function
-const calAge3 = function ( birthYear ) { return 2023 - birthYear }//One line of code
+const calAge3 = function ( birthYear ) { return 2023 - birthYear } //One line of code
 console.log( calAge3( 1990 ) )
 //
 const calAge4 = birthYear => 2023 - birthYear; //One line of code
 const calAge5 = calAge4( 1990 )
 console.log( calAge4( 1990 ) )
+*/
 
 
+/*
 const yearsUntilRetirement = ( birthYear, firstName ) => {
 
     const age = 2023 - birthYear;
@@ -91,3 +101,46 @@ const yearsUntilRetirement = ( birthYear, firstName ) => {
 }
 console.log( "Year of retirement " + yearsUntilRetirement( 1972, "Rommer" ) );
 console.log( "Year of retirement " + yearsUntilRetirement( 1980, "Corazon" ) );
+*/
+
+
+/*
+//--------------------
+
+function cutPieces( fruit ) {
+    return fruit * 4
+}
+
+function fruitProcessor( apples, oranges ) {
+    const applePieces = cutPieces( apples );
+    const orangePieces = cutPieces( oranges );
+    //console.log( apple, orange );
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+    return juice;
+}
+
+console.log( fruitProcessor( 2, 3 ) );
+*/
+
+const calAge = function ( birthYear ) {
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function ( birthYear, firstName ) {
+    const age = calAge( birthYear );
+    const retirement = 65 - age;
+
+    if ( retirement > 0 ) {
+        console.log( `${firstName} retires in ${retirement} years` );
+        return retirement;
+    } else {
+        console.log( `${firstName} retired already` );
+        return -1;
+    }
+
+    //const x = `${firstName} retires in ${retirement} years`;
+    //return retirement;
+}
+console.log( yearsUntilRetirement( 1991, "Sam" ) );
+console.log( yearsUntilRetirement( 1950, "John" ) );
+//console.log( "Year of retirement " + yearsUntilRetirement( 1980, "Bob" ) );
