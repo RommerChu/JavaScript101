322cc8272a5e7066ced0461411979b577109dec9
Rommer Chu <rommer.chu@gmail.com>
2023-04-19 09:15:29 -0400

    code 2 challenge, Array

---
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/arrays.js
@@ -0,0 +1,13 @@
+
+//Array
+
+const friends = [ "Michael", "Steven", "Peter" ]; //literal sentence
+console.log( friends );//to show all items in the array
+console.log( friends[ 2 ] );// to get specific item in the array
+console.log( friends.length ); // shows total number of data in arrays
+console.log( friends[ friends.length - 1 ] ); //get the last item in the arrays
+
+
+
+const years = new Array( 1991, 1992, 1993, 1994, 1995 );
+
