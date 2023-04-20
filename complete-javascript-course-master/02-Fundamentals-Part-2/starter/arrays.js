474ad99213963abe95040c284ffc741e5746eac4
Rommer Chu <rommer.chu@gmail.com>
2023-04-19 21:50:27 -0400

    arrays - working up

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/arrays.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/arrays.js
@@ -1,13 +1,17 @@
 
 //Array
 
-const friends = [ "Michael", "Steven", "Peter" ]; //literal sentence
-console.log( friends );//to show all items in the array
-console.log( friends[ 2 ] );// to get specific item in the array
-console.log( friends.length ); // shows total number of data in arrays
-console.log( friends[ friends.length - 1 ] ); //get the last item in the arrays
+const friends = ["Michael", "Steven", "Peter"]; //literal sentence
+console.log(friends);//to show all items in the array
+console.log(friends[2]);// to get specific item in the array
+console.log(friends.length); // shows total number of data in arrays
+console.log(friends[friends.length - 1]); //get the last item in the arrays
+friends[2] = "Rommer";
+console.log(friends);
 
 
+//const years = new Array(1991, 1992, 1993, 1994, 1995);
 
-const years = new Array( 1991, 1992, 1993, 1994, 1995 );
-
+const firstName = "Rommer";
+const rommer = [firstName, 'Chu', 51, 'Orangeville', friends];
+console.log(rommer);
