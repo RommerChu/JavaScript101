af84de061bf061428aa9aa1f34131f43d9cf4f0e
Rommer Chu <rommer.chu@gmail.com>
2023-04-30 18:44:53 -0400

    start code challenge 4

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/codechallenge.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/codechallenge.js
@@ -7,10 +7,34 @@ const calcTips = function ( bill ) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 }
 
-for ( let b = 0; b <= bills.length; b++ ) {
+console.log(
+    calcTips( bills[ 0 ] ),
+    calcTips( bills[ 1 ] ),
+    calcTips( bills[ 2 ] ),
+    calcTips( bills[ 3 ] ),
+    calcTips( bills[ 4 ] ),
+    calcTips( bills[ 5 ] ),
+    calcTips( bills[ 6 ] ),
+    calcTips( bills[ 7 ] ),
+    calcTips( bills[ 8 ] ),
+    calcTips( bills[ 9 ] ),
+)
 
+tipsies.push(
+    calcTips( bills[ 0 ] ),
+    calcTips( bills[ 1 ] ),
+    calcTips( bills[ 2 ] ),
+    calcTips( bills[ 3 ] ),
+    calcTips( bills[ 4 ] ),
+    calcTips( bills[ 5 ] ),
+    calcTips( bills[ 6 ] ),
+    calcTips( bills[ 7 ] ),
+    calcTips( bills[ 8 ] ),
+    calcTips( bills[ 9 ] ),
+)
 
+console.log( tipsies )
 
-    console.log( calcTips[ b ], bills[ b ] );
-
-}
+// for ( let b = 0; b <= bills.length; b++ ) {
+//     console.log( calcTips[ b ], bills[ b ] );
+// }
