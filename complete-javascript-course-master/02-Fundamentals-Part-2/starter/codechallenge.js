83525eff7fd82103b1e5676cfec89ee38c26b165
Rommer Chu <rommer.chu@gmail.com>
2023-05-01 15:04:35 -0400

    update challenge#4

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/codechallenge.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/codechallenge.js
@@ -1,40 +1,29 @@
 //Coding Challenge #4
-const bills = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86, 52 ];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tipsies = []; // Array of Tips
 const totales = []; // Array of totals
 
-const calcTips = function ( bill ) {
+const calcTips = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 }
 
-console.log(
-    calcTips( bills[ 0 ] ),
-    calcTips( bills[ 1 ] ),
-    calcTips( bills[ 2 ] ),
-    calcTips( bills[ 3 ] ),
-    calcTips( bills[ 4 ] ),
-    calcTips( bills[ 5 ] ),
-    calcTips( bills[ 6 ] ),
-    calcTips( bills[ 7 ] ),
-    calcTips( bills[ 8 ] ),
-    calcTips( bills[ 9 ] ),
-)
 
-tipsies.push(
-    calcTips( bills[ 0 ] ),
-    calcTips( bills[ 1 ] ),
-    calcTips( bills[ 2 ] ),
-    calcTips( bills[ 3 ] ),
-    calcTips( bills[ 4 ] ),
-    calcTips( bills[ 5 ] ),
-    calcTips( bills[ 6 ] ),
-    calcTips( bills[ 7 ] ),
-    calcTips( bills[ 8 ] ),
-    calcTips( bills[ 9 ] ),
-)
+for (let b = 0; b < bills.length; b++) {
 
-console.log( tipsies )
+    //console.log(bills[b]);// bills
+    tipsies.push((calcTips(bills[b])));// tips
+    totales.push(tipsies[b] + bills[b])// tips + bills
+}
+console.log(bills);
+console.log(tipsies);
+console.log(totales);
 
-// for ( let b = 0; b <= bills.length; b++ ) {
-//     console.log( calcTips[ b ], bills[ b ] );
+// function calcAverage(arr) {
+
+//     for (let x = 0; x < bills.length; x++) {
+//         arr = bills[x] + bills[x][x].length;
+
+//         console.log(calcAverage(bills))
+//     }
+
 // }
