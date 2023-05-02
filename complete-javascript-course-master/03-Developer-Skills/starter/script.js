de1348bf18e0fc7c0d32b1fc11b32a753ac36c0f
Rommer Chu <rommer.chu@gmail.com>
2023-05-02 18:40:42 -0400

    prettier

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -1,3 +1,9 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+if (x === 23) console.log(x);
+
+const v = birthyear => 2023 - birthyear;
+
+console.log(v(1980));
