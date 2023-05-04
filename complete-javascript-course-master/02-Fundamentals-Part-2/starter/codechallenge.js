3ad4ea4a5dae51e373afe7d76ad32e2e61828444
Rommer Chu <rommer.chu@gmail.com>
2023-05-03 22:57:36 -0400

    pseudo-code, disect solution

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/codechallenge.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/codechallenge.js
@@ -1,34 +1,34 @@
 //Coding Challenge #4
-const bills = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86, 52 ];
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tipsies = []; // Array of Tips
 const totales = []; // Array of totals
 
-const calcTips = function ( bill ) {
+const calcTips = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
 }
 
 
-for ( let b = 0; b < bills.length; b++ ) {
+for (let b = 0; b < bills.length; b++) {
     //console.log(bills[b]);// bills
-    const cb = calcTips( bills[ b ] );
-    tipsies.push( cb );// tips
-    totales.push( tipsies[ b ] + bills[ b ] )// tips + bills
+    const cb = calcTips(bills[b]);
+    tipsies.push(cb);// tips
+    totales.push(tipsies[b] + bills[b])// tips + bills
 }
-console.log( bills );
-console.log( tipsies );
-console.log( totales );
+console.log(bills);
+console.log(tipsies);
+console.log(totales);
 
 
 
 
 //----------BONUS CHALLENGE
-function calcAverage( arr ) {
+function calcAverage(arr) {
 
     let sum = 0;
 
-    for ( let x = 0; x < arr.length; x++ ) {
+    for (let x = 0; x < arr.length; x++) {
         //sum = sum + arr[ x ];// OR
-        sum += arr[ x ];
+        sum += arr[x];
     }
 
     //console.log( sum )   // Cannot use console after declared value
@@ -36,7 +36,7 @@ function calcAverage( arr ) {
 }
 //calcAverage( [ 2, 3, 7 ] ) //Test the values
 
-console.log( calcAverage( [ 2, 3, 7 ] ) );
-console.log( calcAverage( totales ) );
-console.log( calcAverage( tipsies ) );
-console.log( calcAverage( bills ) );
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totales));
+console.log(calcAverage(tipsies));
+console.log(calcAverage(bills));
--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -4,6 +4,45 @@
 const x = 23;
 if (x === 23) console.log(x);
 
-const v = birthyear => 2023 - birthyear;
+const v = (birthyear) => 2023 - birthyear;
 
-console.log(v(1980));
+console.log(v(1972));
+
+//PROBLEM
+// We work for the company building a smart home thermometer. Our most recent task is this: "Given an array of temperature of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+//1) Understand the problem
+// - What is temp amplitude? Anwer: difference between highest and lowest temp
+// - How to compute max and min temperature?
+// - What is a sensor error? And what to do?
+
+//2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find Max value in temp array
+// - Find Min value in temp array
+// - Subtract min from max (amplitude) and return it
+
+const calcTempAmplitude = (temps) => {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const currentTemp = temps[i];
+
+    if (typeof currentTemp !== 'number') continue; // Ignore the "Sensor Error" string from array
+
+    if (currentTemp > max) {
+      max = currentTemp;
+    }
+    if (currentTemp < max) {
+      min = currentTemp;
+    }
+  }
+
+  console.log(max, min);
+};
+
+//calcTempAmplitude([5, 12, 4, 23, 1, -5]);
+calcTempAmplitude(temperatures);
