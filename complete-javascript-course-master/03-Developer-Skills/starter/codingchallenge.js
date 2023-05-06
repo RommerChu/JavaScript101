6d764ea9b0245e3b6085eb2a7f046adf4e726f64
Rommer Chu <rommer.chu@gmail.com>
2023-05-05 21:26:53 -0400

    code solution

--- complete-javascript-course-master/03-Developer-Skills/starter/codingchallenge.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/codingchallenge.js
@@ -2,25 +2,39 @@
 //Create a function for printForecast which takes array 'arr' ->string
 //display a string
 
+//PROBLEM DISECTION
+//Array transformed to string, separate by ...
+// wwhat is a X days? => index + 1
+
+//Transform array to string
+//Transform each element to string with Celcius
+//String need to contain day (index + 1)
+//Add ... between elements and start and end of string
+
 //Array of maximum temperatures
 const data1 = [17, 21, 23];
 const data2 = [12, 5, -5, 0, 4];
 
-//String conversion from stackoverflow
-// const str = data1.map(function (e) {//
-//   return e.toString();
-// });
-// console.log( str );
-
+//----------MY SOLUTION...
 //Function Print Forecast
-const printForecast = function (arr) {
-  let days = 0;
+const printForecast1 = function (arr) {
+  let days = 0; // Days counter
   for (let i = 0; i < arr.length; i++) {
-    days++;
-    console.log(`${arr[i]}ºC in ${days} days...`);
+    days++; // days counts
+    console.log(`... ${arr[i]}ºC in ${days} days...`);
   }
   console.log(arr);
 };
+printForecast1(data1);
+printForecast1(data2);
 
+//----------SOLUTION FROM THE CLASS
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str = str + `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log('...' + str);
+};
 printForecast(data1);
 printForecast(data2);
