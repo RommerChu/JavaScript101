2ff36c8aef2a015600ee24ce8e7bc045dc487949
Rommer Chu <rommer.chu@gmail.com>
2023-05-18 08:54:00 -0400

    Pig Game completed

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -23,16 +23,16 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-currentScore0El.textContent = 0;
-currentScore1El.textContent = 0;
-diceEl.classList.add('hidden');
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// currentScore0El.textContent = 0;
+// currentScore1El.textContent = 0;
+// diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// const scores = [0, 0];
+// let currentScore = 0;
+// let activePlayer = 0;
+// let playing = true;
 
 //Switch player function
 const switchPlayer = function () {
@@ -43,18 +43,31 @@ const switchPlayer = function () {
   player1.classList.toggle('player--active');
 };
 
+//Scoping
+let scores, currentScore, activePlayer, playing;
+
 //Reset game function
 const reset = function () {
-  currentScore0El.textContent = 0;
-  currentScore1El.textContent = 0;
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
   score0El.textContent = 0;
   score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
   player0.classList.add('player--active');
   player1.classList.remove('player--active');
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
+
+  playing = true;
 };
+reset(); // Call the function
 
 const winner = function () {
   document
@@ -112,7 +125,4 @@ btnHold.addEventListener('click', function () {
   }
 });
 
-btnNew.addEventListener('click', function () {
-  console.log('reset button');
-  reset();
-});
+btnNew.addEventListener('click', reset);
