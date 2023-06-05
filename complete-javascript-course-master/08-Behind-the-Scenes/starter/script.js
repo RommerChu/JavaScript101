5e60a6ad761da2d979527115e8c2e814d1d7e171
Rommer Chu <rommer.chu@gmail.com>
2023-06-05 10:16:49 -0400

    Scoping, TDZ

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function calAge(birthyear) {
+  const age = 2023 - birthyear;
+
+  function printAge() {
+    let output = `${fName} You are ${age}, born in ${birthyear}`;
+    console.log(output);
+
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true; // Using VAR will convert to global variable
+
+      //Reassignment of variable
+      const fName = 'Surefire';
+      output = 'NEW OUT PUT';
+
+      const str = `You are a millenial ${fName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // else {
+    //   console.log(`You are not a millenial ${fName} age ${age}`);
+    // }
+    console.log(millenial);
+    //console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const fName = 'Rommer';
+calAge(1981);
---
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/tdz.js
@@ -0,0 +1,25 @@
+//Temporal Dead Zone TDZ
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Rommer';
+// let job = 'teacher';
+// const year = 1972;
+
+console.log(addDec(2, 3));
+console.log(addDec2(2, 3));
+console.log(addDec3(2, 3));
+
+//Functions
+//Sample1
+function addDec(a, b) {
+  return a + b;
+}
+//Smaple2
+const addDec2 = function (a, b) {
+  return a + b;
+};
+//Smaple3
+var addDec3 = (a, b) => a + b;
