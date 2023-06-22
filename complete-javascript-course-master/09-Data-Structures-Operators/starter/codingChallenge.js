03a6907db34a48cb18318ad6a7b710a2e7d705e9
Rommer Chu <rommer.chu@gmail.com>
2023-06-22 17:01:46 -0400

    coding challenge

---
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/codingChallenge.js
@@ -0,0 +1,56 @@
+'use strict';
+console.log('--------------------CODING CHALLENGE--------------------');
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//console.log( game );
+
+//Task 1
+const player1 = [game.team1, ...game.players[0]];
+const player2 = [game.team2, ...game.players[1]];
+
+console.log('Team1: ', player1);
+console.log('Team2: ', player2);
+
+//Task2
+const gk = player1[0];
+console.log(`Goal Keepers Name: ${gk}`);
