c52ac5c73dbb10cd10ec7fe4ff116f4128fddca1
Rommer Chu <rommer.chu@gmail.com>
2023-11-22 15:22:13 -0500

    Code challenge #3

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/codingChallenge.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/codingChallenge.js
@@ -193,6 +193,51 @@ for (const sc1 of Object.entries(scorers.name)) {
   console.log(`${sc1[1]}: ${scorers.goals[sc1[0]]}`);
 }
 
-console.log('---Other solutiuon---');
+console.log('---Other solution---');
 
-console.log('Rommer xxxxxxxxxxx Chu');
+console.log('-------Coding Challenge #3--------');
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+console.log(gameEvents);
+console.log(gameEvents.size);
+console.log(gameEvents.values());
+
+// 1. Create an array 'events' of the different game events that happened (no duplicates)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+console.log(events);
+
+//Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const timez = [...gameEvents.keys()].pop();
+console.log('Game times', timez);
+console.log(
+  `An event happened, on average, every ${timez / gameEvents.size} minutes`
+);
+
+//Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game,
+// like this: [ FIRST HALF ] 17:⚽ GOAL
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
